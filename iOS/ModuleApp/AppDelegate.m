2c02087a9dcf30ea3de5d2b949e0e051b14ffb57
lgy881228 <510687394@qq.com>
2020-07-22 17:50:02 +0800

    更换目录

---
+++ iOS/ModuleApp/AppDelegate.m
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.m
+//  iOS
+//
+//  Created by edz on 2020/7/22.
+//  Copyright © 2020 edz. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "EntryViewController.h"
+#import <XEngineSDK/XEngineSDK.h>
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+    EntryViewController *homePageVC = [[EntryViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:homePageVC];
+       
+       
+       self.window.rootViewController = nav;
+    [[XEngineSDK sharedInstance] registerApp:@"" andAppSecret:@"" serverUrl:@""];
+    
+    return YES;
+}
+
+
+
+
+
+@end
