7c0d4dd2b44b8cf3357177f0ea5fb97b37f25cca
zk <liuzq7@gmail.com>
2020-07-27 13:10:30 +0800

    rename

--- h5/src/index.js
+++ h5/src/index.js
@@ -1,8 +1,9 @@
 import xengine from "@zk4/xengine";
-xengine._dubug = xengine._dubug || {};
-xengine._dubug.print= function(args){
+xengine._debug = xengine._debug || {};
+xengine._debug.print= function(args){
   //xengine.bridge.call()
   console.log(args);
 }
+
 
 export default xengine._debug;
